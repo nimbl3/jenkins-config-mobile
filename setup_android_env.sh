36577d2cf5ea29746022eb7a0e5aeac737b68209
Trung Le <cs.leetrung@gmail.com>
2017-11-22 10:33:01 +0700

    Update setup_android_env.sh

--- setup_android_env.sh
+++ setup_android_env.sh
@@ -1,5 +1,5 @@
 echo 'export ANDROID_HOME="/Users/[USER_NAME]/Library/Android/sdk"' >> ~/.bash_profile
 
-export PATH=${PATH}:${ANDROID_HOME}/emulator:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools >> ~/.bash_profile
+echo 'export PATH=${PATH}:${ANDROID_HOME}/emulator:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools' >> ~/.bash_profile
 
 . ~/.bash_profile
