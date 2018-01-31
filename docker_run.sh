e4cfb8471b0bc0718db1ead4dab3142f65f07c7c
Red Planet Hotels <redplanethotels@Reds-Mac-mini-2.local>
2018-01-31 07:57:55 +0000

    Edit some options for RedPlanet setup

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d -v $("pwd")/volume-blueocean:/var/jenkins_home -p 8085:8080 -p 50000:50000 --name=jenkins-blueocean -i jenkinsci/blueocean:1.3.3
+docker run -d -v $("pwd")/volume-blueocean:/var/jenkins_home -p 8081:8080 -p 49001:49001 --name=jenkins-blueocean -i jenkinsci/blueocean
--- setup_android_env.sh
+++ setup_android_env.sh
@@ -2,4 +2,6 @@ echo 'export ANDROID_HOME="/Users/[USER_NAME]/Library/Android/sdk"' >> ~/.bash_p
 
 echo 'export PATH=${PATH}:${ANDROID_HOME}/emulator:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools' >> ~/.bash_profile
 
+echo 'export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home/"' >> ~/.bash_profile
+
 . ~/.bash_profile
