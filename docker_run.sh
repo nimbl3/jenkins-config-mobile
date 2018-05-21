74ea9a9e932fa0b329915ad8753b0d3bd835ab43
Issarapong Poesua <dekablade01@gmail.com>
2018-05-21 11:40:46 +0700

    launch container when startup

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d -v $("pwd")/volume-blueocean:/var/jenkins_home -p 80:8080 -p 50000:50000 --name=jenkins-blueocean -i jenkinsci/blueocean:1.3.3
+docker run --restart=always -d -v $("pwd")/volume-blueocean:/var/jenkins_home -p 80:8080 -p 50000:50000 --name=jenkins-blueocean -i jenkinsci/blueocean:1.3.3
