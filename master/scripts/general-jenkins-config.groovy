c2c2caebd4700f64b0dfcba96155948ab0e9702c
sleepylee <cs.leetrung@gmail.com>
2018-07-31 17:39:36 +0700

    Adjust http jenkins url

--- master/scripts/general-jenkins-config.groovy
+++ master/scripts/general-jenkins-config.groovy
@@ -4,7 +4,7 @@
 import jenkins.model.*
 import jenkins.security.s2m.AdminWhitelistRule
 
-String DEFAULT_JENKINS_URL = "https://localhost:8080"
+String DEFAULT_JENKINS_URL = "http://localhost:8080"
 Jenkins jenkinsInstance = Jenkins.instance
 
 jenkinsInstance.setNumExecutors(0)
